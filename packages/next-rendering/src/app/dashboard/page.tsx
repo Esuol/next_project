ea13bdf29b9c008d82920b11aa1701fd2fab2e3b
berlinen <1292393108@qq.com>
2024-07-10 11:46:21 +0800

    feat: suspend 控制渲染顺序

--- packages/next-rendering/src/app/dashboard/page.tsx
+++ packages/next-rendering/src/app/dashboard/page.tsx
@@ -17,17 +17,35 @@ async function Recommend() {
   return <h1>Hello Recommend</h1>;
 }
 
+// 同步渲染
+// export default function Dashboard() {
+//   return (
+//     <section style={{ padding: '20px' }}>
+//       <Suspense fallback={<p>Loading PostFeed Component</p>}>
+//         <PostFeed />
+//       </Suspense>
+//       <Suspense fallback={<p>Loading Weather Component</p>}>
+//         <Weather />
+//       </Suspense>
+//       <Suspense fallback={<p>Loading Recommend Component</p>}>
+//         <Recommend />
+//       </Suspense>
+//     </section>
+//   );
+// }
+
+// 控制渲染顺序
 export default function Dashboard() {
   return (
     <section style={{ padding: '20px' }}>
       <Suspense fallback={<p>Loading PostFeed Component</p>}>
         <PostFeed />
-      </Suspense>
-      <Suspense fallback={<p>Loading Weather Component</p>}>
-        <Weather />
-      </Suspense>
-      <Suspense fallback={<p>Loading Recommend Component</p>}>
-        <Recommend />
+        <Suspense fallback={<p>Loading Weather Component</p>}>
+          <Weather />
+          <Suspense fallback={<p>Loading Recommend Component</p>}>
+            <Recommend />
+          </Suspense>
+        </Suspense>
       </Suspense>
     </section>
   );
