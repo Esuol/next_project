07157aa6ff975634f1d5073b26e937e77182e4cf
berlinen <1292393108@qq.com>
2024-07-11 17:22:53 +0800

    feat: revalidatePath & revalidateTag;

---
+++ packages/next-rendering/src/app/revalidateTag/page.tsx
@@ -0,0 +1,16 @@
+async function getData() {
+  const res = await fetch('https://api.thecatapi.com/v1/images/search', {
+    next: { tags: ['collection'] },
+  });
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
+
+  return <img src={data[0].url} width="300" />;
+}
