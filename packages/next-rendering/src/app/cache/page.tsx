e481593e590a3caa5bc778685eac310997fd3998
berlinen <1292393108@qq.com>
2024-07-12 17:49:10 +0800

    feat: update

--- packages/next-rendering/src/app/cache/page.tsx
+++ packages/next-rendering/src/app/cache/page.tsx
@@ -1,6 +1,8 @@
 import { cache } from 'react';
 console.log('cache', cache);
 
+
+
 async function getData() {
   // 接口每次调用都会返回一个随机的猫猫图片数据
   const res = await fetch('https://api.thecatapi.com/v1/images/search');
