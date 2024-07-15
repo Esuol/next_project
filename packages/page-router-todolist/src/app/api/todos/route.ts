6ed4f89017472a9fcf4670d489a83ca9c02a7b1d
berlinen <1292393108@qq.com>
2024-07-15 11:37:13 +0800

    feat: impl for page router

---
+++ packages/page-router-todolist/src/app/api/todos/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const data = ['阅读', '写作', '冥想'];
+
+export async function GET() {
+  return NextResponse.json({ data });
+}
+
+export async function POST(request: NextRequest) {
+  const formData = await request.formData();
+  const todo = formData.get('todo') as string;
+  data.push(todo);
+  return NextResponse.json({ data });
+}
