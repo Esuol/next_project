ac9d0ae92b415c138171a1b0451fe1bb6f3fd86d
Esuol <berlinenPlus@gmail.com>
2024-07-26 15:23:21 +0800

    feat: update notes

--- packages/next-notes/app/note/edit/page.tsx
+++ packages/next-notes/app/note/edit/page.tsx
@@ -1,5 +1,6 @@
 import NoteEditor from '@/components/NoteEditor';
 
 export default async function EditPage() {
+  // @ts-ignore
   return <NoteEditor noteId={null} initialTitle="Untitled" initialBody="" />;
 }
--- packages/next-notes/components/Sidebar.tsx
+++ packages/next-notes/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import SidebarNoteList from '@/components/SidebarNoteList';
 import EditButton from '@/components/EditButton';
 import NoteListSkeleton from '@/components/NoteListSkeleton';
+import SidebarSearchField from '@/components/SidebarSearchField';
 
 export default async function Sidebar() {
   return (
@@ -22,6 +23,7 @@ export default async function Sidebar() {
           </section>
         </Link>
         <section className="sidebar-menu" role="menubar">
+          <SidebarSearchField />
           <EditButton noteId={null}>New</EditButton>
         </section>
         <nav>
