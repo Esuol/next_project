64705020d75456130df9b5ef33c55a27391a9fd3
Esuol <berlinenPlus@gmail.com>
2024-07-26 16:16:45 +0800

    feat: update notes

--- packages/next-notes/components/SidebarNoteList.tsx
+++ packages/next-notes/components/SidebarNoteList.tsx
@@ -1,26 +1,21 @@
 import SidebarNoteItem from '@/components/SidebarNoteItem';
+import SidebarNoteListFilter from '@/components/SidebarNoteListFilter';
 import { getAllNotes } from '@/lib/redis';
+import { sleep } from '@/lib/utils';
 
 export default async function NoteList() {
-  const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
   await sleep(2000);
   const notes = await getAllNotes();
 
-  const arr = Object.entries(notes);
-
-  if (arr.length == 0) {
+  if (Object.entries(notes).length == 0) {
     return <div className="notes-empty">{'No notes created yet!'}</div>;
   }
 
   return (
-    <ul className="notes-list">
-      {arr.map(([noteId, note]) => {
-        return (
-          <li key={noteId}>
-            <SidebarNoteItem noteId={noteId} note={JSON.parse(note)} />
-          </li>
-        );
+    <SidebarNoteListFilter>
+      {Object.entries(notes).map(([noteId, note]) => {
+        return <SidebarNoteItem noteId={noteId} note={JSON.parse(note)} />;
       })}
-    </ul>
+    </SidebarNoteListFilter>
   );
 }
