c81942881bf82d3fe52f166e3ee4631ef8f4512e
Esuol <berlinenPlus@gmail.com>
2024-08-14 11:31:42 +0800

    feat: update notes

--- packages/next-mdx-note/app/posts/[id]/page.tsx
+++ packages/next-mdx-note/app/posts/[id]/page.tsx
@@ -1,34 +1,38 @@
-import { compileMDX } from 'next-mdx-remote/rsc';
-import { readFile } from 'node:fs/promises';
-import path from 'path';
+import { allPosts } from 'contentlayer/generated';
+import { useMDXComponent } from 'next-contentlayer/hooks';
+import { notFound } from 'next/navigation';
+import dayjs from 'dayjs';
+import ts from 'typescript';
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    id: post._raw.flattenedPath,
+  }));
+}
+// @ts-ignore
+export const generateMetadata = ({ params }) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.id);
+  if (!post) throw new Error(`Post not found for id: ${params.id}`);
+  return { title: post.title };
+};
 
 // @ts-ignore
-async function getMDXContent(name) {
-  try {
-    const filePath = path.join(process.cwd(), '/posts/', `${name}.mdx`);
-    const contents = await readFile(filePath, { encoding: 'utf8' });
-    return await compileMDX({
-      source: contents,
-      options: { parseFrontmatter: true },
-    });
-  } catch (err) {
-    return null;
-  }
-}
+const Page = ({ params }) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.id);
+  if (!post) notFound();
+  const MDXContent = useMDXComponent(post.body.code);
 
-// @ts-ignore
-export async function generateMetadata({ params, searchParams }, parent) {
-  const res = await getMDXContent(params.id);
-  if (!res) return { title: '' };
-  const { frontmatter } = res;
-  return { title: frontmatter.title };
-}
+  return (
+    <article className="mx-auto max-w-xl py-8 prose prose-slate">
+      <div className="mb-8 text-center">
+        <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
+          {dayjs(post.date).format('DD/MM/YYYY')}
+        </time>
+        <h1 className="text-3xl font-bold">{post.title}</h1>
+      </div>
+      <MDXContent />
+    </article>
+  );
+};
 
-// @ts-ignore
-export default async function Home({ params }) {
-  const res = await getMDXContent(params.id);
-  if (!res) return <h1>Page not Found!</h1>;
-  const { content, frontmatter } = res;
-
-  return <>{content}</>;
-}
+export default Page;
